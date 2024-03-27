35543a68b9ab2c5c2acc6d510b2a579feb1c95c0
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-03-27 23:52:31 +0700

    + get current user + xong BE

--- back_end/routes/authRoute.js
+++ back_end/routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {register, login, pageDefault} =  require('../controllers/authController');
+const {register, login, pageDefault, getCurrentUser} =  require('../controllers/authController');
+const {checkCurrentUser} = require('../middleware/checkCurrentUser');
 const Router = express.Router();
 
 Router.route('/register').post(register);
 Router.route('/login').post(login);
+Router.route('/').get(checkCurrentUser, getCurrentUser);
 
 module.exports = Router;
