033286db9fd8650d553773164bf4cfe7fe4ef1b0
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-03-29 17:35:32 +0700

    + reducer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,20 @@
+import { useReducer } from "react";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Main from "./components/Main";
-import NotFoundPage from "./components/NotFoundPage";
 import Register from "./components/Register";
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'; 
-
+import AppReducer from './reducers/AppReducer';
+import AppContext from "./components/AppContext";
 function App() {
+  const initialState = {
+    user: null,
+    posts: []
+  }
+  const [state, dispatch] = useReducer(AppReducer, initialState);
   return (
     <Router>
+      <AppContext.Provider value={{state, dispatch}}>
       <div className="container">
         <Header />
         <Routes>
@@ -17,6 +24,7 @@ function App() {
           <Route exact path="*" element={ <Navigate to ="/" />} />
         </Routes>
       </div>
+      </AppContext.Provider>
     </Router>
   );
 }
