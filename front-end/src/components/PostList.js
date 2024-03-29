ceefa1722a31d4180305299f82f50fb3a8226351
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-03-29 17:49:52 +0700

    + axios

--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import '../css/Post.css'
 import PostItem from './PostItem';
-
+import axios from "axios";
 export default function PostList(){
+  const getAllPosts = useCallback(async () => {
+    try {
+      const options = {
+        method: "get",
+        url: "/api/v1/posts"
+      }
+      const response = await axios(options);
+      const posts = response.data.data.posts;
+      console.log(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  useEffect(()=> {
+    getAllPosts()
+  }, [getAllPosts])
   return (
     <section className="post-section">
       <div className="post-list">
