3c4bef2117ab34810778973c2d0a04149b400d7f
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-03-30 14:58:36 +0700

    + checkCurrentUser

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Main from "./components/Main";
@@ -6,12 +6,35 @@ import Register from "./components/Register";
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'; 
 import AppReducer from './reducers/AppReducer';
 import AppContext from "./components/AppContext";
+import axios from "axios";
 function App() {
   const initialState = {
     user: null,
     posts: []
   }
   const [state, dispatch] = useReducer(AppReducer, initialState);
+  const checkCurrentUser = useCallback(async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const option = {
+        method: "get",
+        url: "api/v1/auth/",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+      const response = await axios(option);
+      if(response.data.data.user){
+        const {userName} = response.data.data.user;
+        dispatch({type: "CURRENT_USER", payload: {userName}});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+  useEffect(() => {
+    checkCurrentUser()
+  }, [checkCurrentUser]);
   return (
     <Router>
       <AppContext.Provider value={{state, dispatch}}>
